584a7a5a515219b096daa41942d37ed67a773b6d
yura.zharkovsky <yura32000@gmail.com>
2017-07-26 15:17:39 +0300

    added method put to storage store

--- watson_streaming_pipelines.py
+++ watson_streaming_pipelines.py
@@ -49,4 +49,23 @@ def put_to_objectstore(credentials, object_name, my_data, region='dallas'):
     s_subject_token = resp1.headers['x-subject-token']
     headers2 = {'X-Auth-Token': s_subject_token, 'accept': 'application/json'}
     resp2 = requests.put(url=url2, headers=headers2, data = my_data )
+	
+def get_from_objectstorage(credentials, object_name, region='dallas'):
+    """This functions returns a StringIO object containing
+    the file content from Bluemix Object Storage."""
+
+    url1 = ''.join(['https://identity.open.softlayer.com', '/v3/auth/tokens'])
+    data = {'auth': {'identity': {'methods': ['password'], 'password': {'user': {'name': credentials['username'],'domain': {'id': credentials['domain_id']}, 'password': credentials['password']}}}}}
+    headers1 = {'Content-Type': 'application/json'}
+    resp1 = requests.post(url=url1, data=json.dumps(data), headers=headers1)
+    resp1_body = resp1.json()
+    for e1 in resp1_body['token']['catalog']:
+        if(e1['type']=='object-store'):
+            for e2 in e1['endpoints']:
+                        if(e2['interface']=='public'and e2['region']==region):
+                            url2 = ''.join([e2['url'],'/', credentials['container'], '/', object_name])
+    s_subject_token = resp1.headers['x-subject-token']
+    headers2 = {'X-Auth-Token': s_subject_token, 'accept': 'application/json'}
+    resp2 = requests.get(url=url2, headers=headers2)
+    return StringIO(resp2.text)
 

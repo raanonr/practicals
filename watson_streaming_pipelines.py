f27fd64f7d6bfd332725e03c3b26670bf6710d8c
Yura32000 <yura32000@gmail.com>
2017-08-06 16:10:25 +0300

    kafka imports moved to create_messagehub_producer function

--- watson_streaming_pipelines.py
+++ watson_streaming_pipelines.py
@@ -4,9 +4,6 @@ import json
 import pickle
 import numpy as np
 from io import StringIO
-import ssl
-from kafka import KafkaProducer 
-from kafka.errors import KafkaError 
 
 def serializeObject(pythonObj):
     return pickle.dumps(pythonObj, pickle.HIGHEST_PROTOCOL)
@@ -83,6 +80,10 @@ def get_from_objectstore(credentials, object_name, binary=True, region='dallas')
 # !pip install kafka-python
 # https://github.com/dpkp/kafka-python
 def create_messagehub_producer(username, password, kafka_brokers_sasl = [], sasl_mechanism = 'PLAIN', security_protocol = 'SASL_SSL', value_serializer=lambda v: json.dumps(v).encode('utf-8')):
+    import ssl
+    from kafka import KafkaProducer 
+    from kafka.errors import KafkaError 
+
     if (kafka_brokers_sasl == []):
         kafka_brokers_sasl = [
             "kafka01-prod01.messagehub.services.us-south.bluemix.net:9093",

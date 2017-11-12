778da9a4d0b67e5a10e27f45e5e1d97b1291df69
Yura32000 <yura32000@gmail.com>
2017-11-12 14:53:27 +0200

    Added support for COS and message hub consumer

--- watson_streaming_pipelines.py
+++ watson_streaming_pipelines.py
@@ -4,6 +4,7 @@ import json
 import pickle
 import numpy as np
 from io import StringIO
+import os
 
 def serializeObject(pythonObj):
     return pickle.dumps(pythonObj, pickle.HIGHEST_PROTOCOL)
@@ -14,14 +15,14 @@ def deserializeObject(pickledObj):
 def serializeKerasModel(model):
     with NamedTemporaryFile() as f:
         model.save(f.name)
+        f.seek(0)
         obj = f.read() 
     return obj
 
-def deserializeKerasModel(obj):
+def deserializeKerasModel(pickledObj):
     from keras.models import Model,load_model
     with NamedTemporaryFile(mode='wb') as f:
-        f.write(obj)
-        f.flush()
+        f.write(pickledObj)
         model = load_model(f.name)  
     return model
     
@@ -34,7 +35,7 @@ def serializeNumpyArray(arr):
     return obj  
     
 def serializeFile(path):
-    with open(path) as f:
+    with open(path, mode='rb') as f:
         obj = f.read()  
     return obj    
 
@@ -68,16 +69,49 @@ def get_from_objectstore(credentials, object_name, binary=True, region='dallas')
             for e2 in e1['endpoints']:
                         if(e2['interface']=='public'and e2['region']==region):
                             url2 = ''.join([e2['url'],'/', credentials['container'], '/', object_name])
-    s_subject_token = resp1.headers['x-subject-token']
+    s_subject_token = resp1.headers['x-subject-token']_
     headers_accept = 'application/octet-stream' if (binary) else 'application/json' 
     headers2 = {'X-Auth-Token': s_subject_token, 'accept': headers_accept}
     resp2 = requests.get(url=url2, headers=headers2)
     res = resp2.content if (binary) else StringIO(resp2.text)
     return res
+    
+# make sure to install
+def get_cloud_object_storage(apikey, resource_instance_id, auth_endpoint = 'https://iam.bluemix.net/oidc/token', service_endpoint = 'https://s3-api.us-geo.objectstorage.softlayer.net'):
+    import boto3
+    from botocore.client import Config
+    cos = boto3.resource('s3',
+                          ibm_api_key_id = apikey,
+                          ibm_service_instance_id = resource_instance_id,
+                          ibm_auth_endpoint = auth_endpoint,
+                          config=Config(signature_version='oauth'),
+                          endpoint_url = service_endpoint)
+    return cos
+                         
+def put_to_cloud_object_storage(cos, bucket_name, obj, name): 
+    with NamedTemporaryFile() as f:
+        f.write(obj)
+        cos.Bucket(bucket_name).upload_file(f.name, name)
+        
+def get_from_cloud_object_storage(cos, bucket_name, name): 
+    obj = None
+    path = None
+    
+    with NamedTemporaryFile(mode='wb', delete=False) as f:
+        path = f.name
+        cos.Bucket(bucket_name).download_file(name, path) 
+    
+    with open(path, mode='rb') as f:
+        obj = f.read()
+    
+    os.remove(path)
+    
+    return obj
+        
 
-# make sure to install kafka-python library
-# !pip install kafka-python
-# https://github.com/dpkp/kafka-python
+# Make sure to install: ibm-cos-sdk
+# !pip install ibm-cos-sdk
+# https://github.com/IBM/ibm-cos-sdk-python
 def create_messagehub_producer(username, password, kafka_brokers_sasl = [], sasl_mechanism = 'PLAIN', security_protocol = 'SASL_SSL', value_serializer=lambda v: json.dumps(v).encode('utf-8')):
     import ssl
     from kafka import KafkaProducer 
@@ -106,3 +140,33 @@ def create_messagehub_producer(username, password, kafka_brokers_sasl = [], sasl
                              value_serializer=value_serializer)
                              
     return producer
+    
+    
+def create_messagehub_consumer(username, password, kafka_brokers_sasl = [], sasl_mechanism = 'PLAIN', security_protocol = 'SASL_SSL', value_deserializer=lambda v: json.loads(v).encode('utf-8')):
+    import ssl
+    from kafka import KafkaConsumer 
+    from kafka.errors import KafkaError 
+
+    if (kafka_brokers_sasl == []):
+        kafka_brokers_sasl = [
+            "kafka01-prod01.messagehub.services.us-south.bluemix.net:9093",
+            "kafka02-prod01.messagehub.services.us-south.bluemix.net:9093",
+            "kafka03-prod01.messagehub.services.us-south.bluemix.net:9093",
+            "kafka04-prod01.messagehub.services.us-south.bluemix.net:9093",
+            "kafka05-prod01.messagehub.services.us-south.bluemix.net:9093" 
+        ] 
+
+    # Create a new context using system defaults, disable all but TLS1.2
+    context = ssl.create_default_context()
+    context.options &= ssl.OP_NO_TLSv1
+    context.options &= ssl.OP_NO_TLSv1_1
+
+    consumer = KafkaConsumer(bootstrap_servers = kafka_brokers_sasl,
+                             sasl_plain_username = username,
+                             sasl_plain_password = password,
+                             security_protocol = security_protocol,
+                             ssl_context = context,
+                             sasl_mechanism = sasl_mechanism,
+                             value_deserializer=value_deserializer)
+                             
+    return consumer
